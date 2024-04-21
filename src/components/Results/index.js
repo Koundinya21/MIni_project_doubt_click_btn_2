b1d6d7d306b842721738ce30f0c7ad08ace6e31e
koundinya21 <149105895+Koundinya21@users.noreply.github.com>
2024-04-22 01:32:01 +0530

    Update index.js

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,37 +1,71 @@
 import './index.css'
+// import {useContext} from 'react'
 import {Link} from 'react-router-dom'
 import Header from '../Header'
+import DetailsContext from '../../ReactContext'
+import TimeUp from '../TimeUp'
+
 // import ScoreContext from '../../ReactContext'
 
-const Results = ({location}) => {
-  const {score} = location.state
-  //   const {completedTime} = props
+// const Results = ({location}) => {
+//   const {score} = location.state
+//   const {completedTime} = props
+const Results = props => {
+  const {history} = props
+  const {location} = history
+  const {score, isTimeRunning} = location.state
 
   return (
-    <>
-      <Header />
-      <div className="bg-container">
-        <div className="component">
-          <div className="result-card-container">
-            <div className="bg-card">
-              <img
-                src="https://res.cloudinary.com/dowxofd2k/image/upload/v1711890610/Asset_2_1_c8uttm.png"
-                alt="submit"
-                className="img-submit"
-              />
-              <p className="para">Congrats! You completed the assessment.</p>
-              <p className="time-taken">Time Taken: 00</p>
-              <p className="score">Your Score: {score}</p>
-              <Link to="/assess/questions">
-                <button type="button" className="reattempt-btn">
-                  Reattempt
-                </button>
-              </Link>
+    <DetailsContext.Consumer>
+      {value => {
+        const {elapsedTimeInSeconds} = value
+
+        const hours = Math.floor(elapsedTimeInSeconds / 3600)
+        const minutes = Math.floor((elapsedTimeInSeconds % 3600) / 60)
+        const seconds = Math.floor(elapsedTimeInSeconds % 60)
+        const disHr = hours > 9 ? hours : `0${hours}`
+        const disMin = minutes > 9 ? minutes : `0${minutes}`
+        const disSec = seconds > 9 ? seconds : `0${seconds}`
+        const formattedTime = `${disHr}:${disMin}:${disSec}`
+
+        const scoreValue = score === 10 ? {score} : `0${score}`
+
+        return isTimeRunning ? (
+          <div>
+            <Header />
+            <div className="bg-container">
+              <div className="component">
+                <div className="result-card-container">
+                  <div className="bg-card">
+                    <img
+                      src="https://res.cloudinary.com/dowxofd2k/image/upload/v1711890610/Asset_2_1_c8uttm.png"
+                      alt="submit"
+                      className="img-submit"
+                    />
+                    <p className="para">
+                      Congrats! You completed the assessment
+                    </p>
+                    <p className="time-taken">Time Taken: {formattedTime}</p>
+                    <p className="score">Your score:{scoreValue}</p>
+                    <Link to="/assessment">
+                      <button type="button" className="reattempt-btn">
+                        Reattempt
+                      </button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      {/* <div className="bg-container">
+        ) : (
+          <TimeUp score={score} />
+        )
+      }}
+    </DetailsContext.Consumer>
+  )
+}
+
+/* <div className="bg-container">
         <div className="component">
           <div className="results-card_container">
             <img
@@ -50,10 +84,8 @@ const Results = ({location}) => {
             </Link>
           </div>
         </div>
-      </div> */}
-    </>
-  )
-}
+      </div> */
+
 //   <ScoreContext.Consumer>
 //     {value => {
 //       const {score} = value
